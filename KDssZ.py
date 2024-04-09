7ca52f2711da1bacd926bb89092bb46888eca9f0
Daniel Andrés Triana Camacho <47016495+dantrica@users.noreply.github.com>
2024-04-09 08:26:54 +0200

    Add files via upload

---
+++ KDssZ.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 22:03:30 2024
+
+@author: Daniel Andrés Triana Camacho
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from scipy import optimize
+from os.path import join
+
+class KDssZ:
+    freqs = []
+    impedances = []
+    labels = []
+    concent = []
+    def __init__(self):
+        self.instance_variable = []
+        self.freqs = []
+        self.impedances = []
+        self.labels = []
+        self.concent = []
+     
+    # fn: archivos seleccionados en la app, metadata: todos los archivos
+    def call_data(self, fn, files_txt):
+        for uf in files_txt:
+            if uf.name in fn:
+                df = pd.read_csv(uf, sep='\t')
+                f = df["Frequency (Hz)"]
+                Z = df["Z' (Ω)"].to_numpy() - 1j * df["-Z'' (Ω)"].to_numpy()
+                self.freqs.append(f)
+                self.impedances.append(Z)
+                self.labels.append(uf.name)
+                
+    def cole_cole(self, f, Xi, DX, tau, alpha):
+        return Xi + DX / ( 1 + (2j*np.pi*f*tau)**(1-alpha) )
+    
+    
+    def optimize_tau(self, f, X, Xi, DX, tau, alpha, algorith='least_squares'):
+        def err(T):
+            if T < 0:
+                return 100
+            X1 = self.cole_cole(f, Xi, DX, T, alpha)
+            return sum(np.abs(X1 - X)**2)/sum(np.abs(X)**2)
+        opt = np.nan
+        if 'least_squares' in algorith:
+            opt = optimize.least_squares(err, tau, bounds=(0, np.inf))
+        if 'basinhopping' in algorith:
+            opt = optimize.basinhopping(err, tau, niter=1000)
+        return opt.x[0]
+    
+    def get_cole_params(self, f, X, algorith='least_squares', verbose=True):
+        def f_2b(c):
+            """ calculate the algebraic distance between the 2D points and the mean circle centered at c=(-xc, -yc, C) """
+            A, B, C = c
+            Xi = 100
+            if A**2 >= C:
+                R = np.sqrt(A**2 + B**2 - C)
+                Xi = A + np.sqrt( A**2 - C )
+            x = np.real(X)
+            y = np.imag(X)
+            r = x**2 + y**2 + 2*x*A + 2*y*B + C
+            if Xi < 0:
+                return 10*r
+            return r
+        x_m = np.mean(np.real(X))
+        R0  = ( max(np.real(X)) - min(np.real(X)) )/2.0
+        y_m = max(np.imag(X)) - R0
+        C0  =  x_m**2 + y_m**2 - R0**2
+        ABC = -x_m, -y_m, C0
+        ABC_opt, ier = optimize.leastsq(f_2b, ABC)
+        A, B, C = ABC_opt
+        R = np.sqrt(A**2 + B**2 - C) # (y + B)^2 + (x + A)^2 = R^2
+        if R**2 < B**2:
+            if verbose:
+                print('Warning! The Cole-Cole inversion fails.')
+            return np.nan, np.nan, np.nan, np.nan
+        DX = 2 * np.sqrt( R**2 - B**2 )
+        Xi1 = -A - DX/2
+        Xi2 =  A + DX/2
+        Xmax = R - B
+        phi = np.arctan(-2*B/DX)
+        alpha = 2 * phi / np.pi
+
+        def get_tau(Xi):
+            # Obtention of tau #
+            k = min(range(len(f)), key=lambda i: abs(-np.imag(X)[i] - Xmax))
+            fc = f[k]
+            if type(fc) == type([]):
+                fc = fc[0]
+            if DX/(DX+Xi) < 0:
+                return 1
+            return np.sqrt(DX/(DX+Xi))/(2*np.pi*fc)
+            
+        X1 = self.cole_cole(f, Xi1, DX, get_tau(Xi1), alpha)
+        X2 = self.cole_cole(f, Xi2, DX, get_tau(Xi2), alpha)
+        Xi = Xi1
+        tau = get_tau(Xi1)
+        if sum(np.abs(X1 - X)**2) > sum(np.abs(X2 - X)**2):
+            Xi = Xi2
+            tau = get_tau(Xi2)
+    
+        tau_opt = self.optimize_tau(f, X, Xi, DX, tau, alpha, algorith=algorith)
+        X1 = self.cole_cole(f, Xi1, DX, tau, alpha)
+        X2 = self.cole_cole(f, Xi2, DX, tau_opt, alpha)
+        if not np.isnan(tau_opt) and sum(np.abs(X1 - X)**2) > sum(np.abs(X2 - X)**2):
+            tau = tau_opt
+
+        return Xi, DX, tau, alpha
+    
+    def impedance_model(self, f, p):
+        return self.impedance_model_opt(f, p[4:], p[:4])
+
+    def impedance_model_opt(self, f, p, cole_cole_fixed_params):
+        #Rq, T, a, Ro, To, a0 = p
+        other_fixed_params = p[3:]
+        return self.impedance_model_two(f, p[:3], cole_cole_fixed_params, other_fixed_params)
+
+    def impedance_model_one(self, f, p, cole_cole_fixed_params):
+        Ro, To, a0 = p
+        Xi, DX, tau, alpha = cole_cole_fixed_params
+        Z1 = self.cole_cole(f, Xi, DX, tau, alpha)
+        ZZ = Ro/(2j * f * To)**(1 - a0)
+        return  Z1 + ZZ
+
+    def impedance_model_two(self, f, p, cole_cole_fixed_params, other_fixed_params):
+        Rq, T, a = p
+        Xi, DX, tau, alpha = cole_cole_fixed_params
+        Ro, To, a0 = other_fixed_params
+        #Z1 = impedance_model_one(f, other_fixed_params, cole_cole_fixed_params)
+        Z1 = self.cole_cole(f, Xi, DX, tau, alpha)
+        ZZ = Ro/(2j * f * To)**(1 - a0)
+        ZC = Rq/(2j * f * T)**a
+        return  Z1 + ZC + ZZ
+
+    def optimize1_model(self, f, Z, x0, cole_cole_fixed_params):
+        """ optimize_model determina los parámetros de un modelo de fase constante tipo capactivio
+            con Z = Rq/(jwT)^a, donde T = Rq Cq. cole_Cole_fixed_params son parámetros del modelo de cole-cole
+            obtenido con los datos de impedancia, se dejan fijos.
+            x0 = [tau_c/(DC+Ci), tau_c, alpha_c], donde tau_c, DC, Ci y alpha_c son obtenidos del modelo de cole-cole
+            aplicado a la capacitancia compleja C = 1/(jwZ)
+        """
+        if any(np.isnan(x0)):
+            return 6*[np.nan]
+        def err1(p):
+            a = p[2]
+            if any( p < 0 ) or a > 1:
+                return 100 
+            ZZ = self.impedance_model_one(f, p, cole_cole_fixed_params)
+            return sum(np.abs(Z - ZZ)**2)/sum(np.abs(Z)**2)
+
+        opt = optimize.least_squares(err1, np.array(x0)[:3], bounds=(0, np.inf))
+        #Rq, T, a = opt.x
+        def err2(p):
+            a = p[2]
+            if any( p < 0 ) or a > 1:
+                return 100 
+            ZZ = self.impedance_model_two(f, p, cole_cole_fixed_params, opt.x)
+            return sum(np.abs(Z - ZZ)**2)/sum(np.abs(Z)**2)
+        #Ro, To, a0 = x0[:3]
+        opt2 = optimize.least_squares(err2, np.array(x0[3:]), bounds=(0, np.inf))
+        return np.concatenate([opt2.x, opt.x])
+
+    def optimize_model(self, f, Z, x0, cole_cole_fixed_params):
+        if any(np.isnan(x0)):
+            return 6*[np.nan]
+        if any(np.isnan(cole_cole_fixed_params)):
+            return 6*[np.nan]
+        p0 = self.optimize1_model(f, Z, x0, cole_cole_fixed_params)
+        Rq, T, a, Ro, To, a0 = p0
+        def err(p):
+            if any( p < 0 ):
+                return 100
+            this_p = [Rq, p[0], a, Ro, p[1], a0]
+            ZZ = self.impedance_model_opt(f, this_p, cole_cole_fixed_params)
+            return sum(np.abs(Z - ZZ)**2)/sum(np.abs(Z)**2)
+    
+        T_To = optimize.least_squares(err, [T, To], bounds=(0, np.inf))
+        return [Rq, T_To.x[0], a, Ro, T_To.x[1], a0]
+
+    def get_linear_slope(self, x, y):
+        err = lambda slope: sum( (slope*x - y)**2 )
+        opt = optimize.least_squares(err, 1, bounds=(0, np.inf))
+        return opt.x[0]
+
+    def model(self, i, f, Z, label, f_hight=25000, f_middle=10000, 
+                       f_low=1, f_=np.logspace(-2, 8, 2000), f2=np.logspace(-5, 8, 2000)):
+        Xi, DX, tau, alpha = self.get_cole_params(f[f>f_hight], Z[f>f_hight])
+    
+        Z_ = self.cole_cole(f_, Xi, DX, tau, alpha) # Xi + DX / ( 1 + (2j*np.pi*f_*tau)**(1-alpha) )#
+        x = np.real(Z)[f<f_low]
+        y = -np.imag(Z)[f<f_low]
+        p = np.polyfit(x, y, 1)
+        x_ = np.linspace(-p[1]/p[0], max(x), 10)
+    
+        x1 = np.real(Z)[(f>f_low) & (f<f_middle)]
+        y1 = -np.imag(Z)[(f>f_low) & (f<f_middle)]
+        slope1 = self.get_linear_slope(x1, y1)
+        p1 = [slope1, 0]
+        #p1 = np.polyfit(x1, y1, 1)
+        print('Medium frequency phenomena (slope, intercept)', p1)
+
+        C = 1 / (2j * np.pi * f * Z)
+    
+        Ci, DC, tau_c, alpha_c = self.get_cole_params(f, C)
+        cole_cole_fixed_params = [Xi, DX, tau, alpha]
+        CPE_params = [max(np.real(Z)), 1, 2 * np.arctan(1/slope1) / np.pi] # CPE start parameters: Zo, tau, alpha
+        x0 = CPE_params + [tau_c/(DC+Ci), tau_c, alpha_c] # initial params to the model CPE_params + [ Ro, tau, alpha_c ]
+    
+        p_model = self.optimize_model(f, Z, x0, cole_cole_fixed_params)
+        opt_params = np.concatenate([cole_cole_fixed_params, p_model])
+
+        if any(np.isnan(opt_params)):
+            return 10*[np.nan]
+        
+        Z2 = self.impedance_model(f_, opt_params) # datos del modelo
+    
+        C_ = self.cole_cole(f2, Ci, DC, tau_c, alpha_c)
+        C2 = 1 / (2j * np.pi * f_ * Z2)
+        C1 = 1 / (2j * np.pi * f_ * Z_)
+
+        return opt_params, Z2, f_
---
+++ appZ.py
@@ -0,0 +1,269 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 22:34:44 2024
+
+@author: Daniel A- Triana-Camacho
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from KDssZ import *
+
+# Instantiating the class KDssZ()
+KDss = KDssZ()
+
+# Setting the app to occupy the entire width of the screen
+st.set_page_config(layout="wide")
+
+# Welcome message on the title
+title = 'Welcome to the key data source since impedance (KDssZ)'
+st.title(title)
+
+# Software description in brief
+st.write("This is a software tool for electrical impedance analysis of cement-based composites")
+st.write('---')
+
+# Adding selected_filename to the application sesion
+if 'selected_filename' not in st.session_state:
+    st.session_state.selected_filename = []
+
+#------------------------------------------------------------------------------
+# Building the left option sidebar
+with st.sidebar:
+    
+    sidebar_title = "KDssZ Options"
+    st.sidebar.title(sidebar_title)
+    
+    # file_uploader to get the .txt impedance data
+    files_txt = st.file_uploader(
+        label="Upload Z data", 
+        accept_multiple_files=True,
+        help='''Upload all .txt files that contains impedance measurements 
+            from PGSTAT206 by AUTOLAB, and those were extracted with 
+            the software NOVA 2.0'''
+        )
+    
+    # Asking if is there elements in the list
+    if len(files_txt) > 0:
+        
+        # adding elements from files_txt to the list file_names
+        file_names = []
+        for j in files_txt:
+            file_names.append(j.name)
+        
+        # The box contains a list with the file names st.selectbox('text', list)
+        file_box = st.selectbox(
+            'Measurements',
+            file_names,
+            label_visibility="visible",
+            help='''Select here the measure to add or remove in the 
+                optimization zone'''
+            )
+    
+        # Create two columns
+        sidebar_col1, sidebar_col2 = st.columns(2)
+    
+        # Button to add Z data, column 1
+        with sidebar_col1:
+            with st.container(height=180):
+                if st.button('Add data', help="Add chosen data file"):
+                    # Warning, don't add the same file twice
+                    if file_box in st.session_state.selected_filename:
+                        st.write('''The file already was added, please 
+                                 chose other''')
+                    else:
+                        st.session_state.selected_filename.append(file_box)
+    
+        # Button to remove Z data, column 2
+        with sidebar_col2:
+            with st.container(height=180):
+                if st.button('Remove', help='''Remove data file previoulsy 
+                             added'''):
+                    # Warning, There are not more files to remove
+                    if len(st.session_state.selected_filename) == 0:
+                        st.write('The list is empty, please add a file')
+                    else:
+                        if file_box not in st.session_state.selected_filename:
+                            st.write('''The file is not on the list, please 
+                                     try removing other''')
+                        else:
+                            st.session_state.selected_filename.remove(file_box)
+
+        # Updating the list of Z files
+        with st.container(height=200):
+            st.write('Optimization zone:', st.session_state.selected_filename)
+    
+        #----------------------------------------------------------------------
+        # Buttons Run and Reset to start and delete 
+        # the optimization, respectively
+    
+        # Create two columns
+        sidebar_col3, sidebar_col4 = st.columns(2)
+    
+        # Button Run, Calling the method KDss.call_data to save the information
+        # coming from .txt files in global variables of KDss
+        with sidebar_col3:
+            if st.button('Run', help="Run the optimization algoritm"):
+                'Optimizing'
+                fn_chose = st.session_state.selected_filename # fn: file name
+                # KDss.call_data(fn_chose, df)
+                KDss.call_data(fn_chose, files_txt)
+
+        with sidebar_col4:
+            if st.button('Reset', help="Clear the paramenters table and figures"):
+                'we are working on'
+                st.session_state.selected_filename.clear()
+    
+    #--------------------------------------------------------------------------
+    # Presenting the figure of the lumped circuital model
+    imagen = 'figures/circuital_model.png'
+    if os.path.exists(imagen):
+        st.image(
+            imagen, caption='Model: three frequencies mode',
+            use_column_width=True
+            )
+    else:
+        st.write('The image is not in the filepath: '+imagen)
+
+#------------------------------------------------------------------------------
+fig1 = plt.figure()
+fig2 = plt.figure()
+
+# Container with the Nyquist and Bode diagrams
+with st.container():    
+       
+    param_labels = [
+    "Ri [Ω]",
+    "R1-Ri [Ω]",
+    "𝜏1 [s] | hight freq",
+    "α1 | hight freq",
+    "R3 [Ω] | low freq",
+    "𝜏3 [s] | low freq",
+    "α3 | low freq",
+    "R2 [Ω] | middle freq",
+    "𝜏2 [s] | middle freq",
+    "α2 | middle freq"
+    ]
+    model_data = {
+        'sample':[]
+    }
+    for p in param_labels:
+        model_data[p] = []
+        
+    params = []
+    fdata = []
+    Zreal = []
+    Zimag = []
+    f_model = []
+    Zreal_model = []
+    Zimag_model = []
+    for i, label in enumerate(KDss.labels):
+        print(i)
+        f = KDss.freqs[i] 
+        Z = KDss.impedances[i]
+        p = 10*[np.nan]
+        if max(f) > 100e3:
+            p, Z_model, fm = KDss.model(i, f, Z, f_hight=25e3, f_middle=10e3, f_low=1, label=label)
+            
+        params.append(p)
+        model_data['sample'].append(KDss.labels[i])
+        for key, val in enumerate(p):
+            this_val = val
+            if key == 6:
+                this_val = 1 - val
+            model_data[param_labels[key]].append(this_val)
+        fdata.append(f)
+        Zreal.append(np.real(Z)*1e-3)
+        Zimag.append(-np.imag(Z)*1e-3)
+        
+        if 'fm' in globals():
+            f_model.append(fm)
+            Zreal_model.append(np.real(Z_model)*1e-3)
+            Zimag_model.append(-np.imag(Z_model)*1e-3)
+        else:
+            st.write('This data file was not optimized')
+            f_model.append([])
+            Zreal_model.append([])
+            Zimag_model.append([])
+    
+    new_fdata = list(zip(*fdata))
+    new_Zreal = list(zip(*Zreal))
+    new_Zimag = list(zip(*Zimag))
+    new_f_model = list(zip(*f_model))
+    new_Zreal_model = list(zip(*Zreal_model))
+    new_Zimag_model = list(zip(*Zimag_model))
+    
+    # Column format to present the Nyquist and Bode diagrams
+    sidebar_col5, sidebar_col6 = st.columns(2)
+    
+    # Nyquist plot
+    with sidebar_col5:
+        fig1 = plt.figure(1)
+        
+        if len(KDss.labels) == 0:
+            sl = ''
+        elif len(KDss.labels) == 1:
+            sl = KDss.labels[0], 'Model Fitting'
+        else:
+            sl = []
+            for sj in KDss.labels:
+                sl.append(sj)
+            sl.append('Model Fitting')
+        
+        plt.plot(new_Zreal, new_Zimag, linestyle = 'None', marker='o', markerfacecolor='None')
+        plt.plot(new_Zreal_model, new_Zimag_model, 'k:')
+        plt.gca().set_aspect('equal', 'box')
+        plt.xlabel(r"real(Z) [k$\Omega$]")
+        plt.ylabel(r"-imag(Z) [k$\Omega$]")
+        plt.ylim(0, 50)
+        plt.xlim(0, 50)
+        plt.legend(labels=sl)
+        
+        st.pyplot(fig1)
+    
+    # Bode plot
+    with sidebar_col6:
+        
+        fig2 = plt.figure(2)
+        plt.subplot(2,1,1)
+        plt.semilogx(new_fdata, new_Zreal, linestyle = 'None', marker='o', markerfacecolor='None')
+        plt.semilogx(new_f_model, new_Zreal_model, 'k:')#, lw=2, markerfacecolor='None', alpha=0.7)
+        plt.subplot(2,1,2)
+        plt.semilogx(new_fdata, new_Zimag, linestyle = 'None', marker='o', markerfacecolor='None')
+        plt.semilogx(new_f_model, new_Zimag_model, 'k:')#, lw=2, markerfacecolor='None', alpha=0.7)
+        #----------------------------------------------------------------------
+        plt.subplot(2,1,1)
+        plt.xlim(0.1, 1e6)
+        plt.ylim(0, 30)
+        plt.xlabel('Frequency [Hz]')
+        plt.ylabel(r"real(Z) [k$\Omega$]")
+        plt.gca().yaxis.set_label_position("right")
+        plt.gca().yaxis.tick_right()
+        plt.legend(labels=sl)
+        #----------------------------------------------------------------------
+        plt.subplot(2,1, 2)
+        plt.xlim(0.1, 1e6)
+        plt.ylim(0, 10)
+        plt.xlabel('Frequency [Hz]')
+        plt.ylabel(r"-imag(Z)  [k$\Omega$]")
+        plt.gca().yaxis.set_label_position("right")
+        plt.gca().yaxis.tick_right()
+        plt.legend(labels=sl)
+        
+        st.pyplot(fig2)
+        
+        # st.scatter_chart(pd.DataFrame(new_Zreal_model))
+        
+columns = [param_labels[i] for i in [0, 1, 2, 3, 7, 8, 9, 4, 5, 6]]
+model_df = pd.DataFrame(model_data, columns=columns)
+
+
+# DataFrame which contains the optimized parameters
+with st.container():
+    edited_model_df = st.data_editor(
+        model_df,
+        hide_index=True,
+    )
