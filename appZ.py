492db9097b47e1c54c483a9f38a3d670708edb65
Daniel Andrés Triana Camacho <47016495+dantrica@users.noreply.github.com>
2024-06-13 15:20:05 +0200

    Update appZ.py

--- appZ.py
+++ appZ.py
@@ -5,10 +5,10 @@ Created on Sun Mar  3 22:34:44 2024
 @author: Daniel A- Triana-Camacho
 
 The AppZ is a window for inputting electrical impedance data into 
-the Key Data Source Since impedance (KDssZ) library to optimize a 
-three-part lumped circuit model. The App provides Nyquist and Bode 
-plots along with a table that contains the optimized parameters 
-from the model.
+the Key Data Source Sampling impedance measurements (KDssZ) library 
+to optimize a three-part lumped circuit model. The App provides 
+Nyquist and Bode plots along with a table that contains the optimized
+parameters from the model.
 """
 
 import streamlit as st
