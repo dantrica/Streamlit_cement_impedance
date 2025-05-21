bfdb6f96c5424ef63aa6b1e458ff05d185ee12fc
Daniel Andrés Triana Camacho <47016495+dantrica@users.noreply.github.com>
2025-05-21 08:47:03 +0200

    Update KDssZ.py

--- KDssZ.py
+++ KDssZ.py
@@ -187,7 +187,7 @@ class KDssZ:
         return opt.x[0]
 
     def model(self, i, f, Z, label, f_hight=25000, f_middle=10000, 
-                       f_low=0.11, f_=np.logspace(-2, 8, 2500), f2=np.logspace(-5, 8, 2500)):
+                       f_low=0.11, f_=np.logspace(-2, 8, 2000), f2=np.logspace(-5, 8, 2000)):
         
         Xi, DX, tau, alpha = self.get_cole_params(f[f>f_hight], Z[f>f_hight])
         Z_ = self.cole_cole(f_, Xi, DX, tau, alpha) # Xi + DX / ( 1 + (2j*np.pi*f_*tau)**(1-alpha) )#
